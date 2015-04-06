ea6c28132694ac8fe075d23e2ce14ea889b3ec6f
Masumi Hayashi-Smith <masumi.hs@gmail.com>
2015-04-06 09:33:56 -0700

    connect app.js to json files and refactor vocabulary

--- JS/app.js
+++ JS/app.js
@@ -1,98 +1,100 @@
-(function(){
-
-	var initialSongs = [
-		{
-			songName: 'All around the kitchen (Cocky-doodle-doodle-do)',
-			songUrl: 'http://kodaly.hnu.edu/song.cfm?id=599',
-			key: 'A / l,t,Drma'
-		},
-
-		{
-			songName: 'Apple Tree',
-			songUrl: 'http://emilyskodalymusic.blogspot.com/2013/05/apple-tree.html',
-			key: 'D / Dmsl'
-		},
-
-		{
-			songName: 'Baker shop (Down to the)',
-			songUrl: 'http://kodaly.hnu.edu/song.cfm?id=907',
-			key: 'D / Drmfsl'
-		}
-	];
-
-	var initialLists = [
-		{
-			name: "list1"
-		},
-
-		{
-			name: "list2"
-		}
-	];
-
-
-
-	var Song = function (data) {
-		this.songName = ko.observable(data.songName);
-		this.songUrl = ko.observable(data.songUrl);
-		this.key = ko.observable(data.key);
+// (function(){
+	var app = {
+		model: {}
 	};
 
+	var d1 = $.Deferred();
+	var d2 = $.Deferred();
+	var j1 = $.getJSON("semesters.json").done(function(data){
+		console.log('poop');
+		app.model.semesters = data;
+		d1.resolve();
+	});
+	var j2 = $.getJSON("songs.json").done(function(data){
+		console.log('poop');
+		app.model.songs = data;
+		d2.resolve();
+	});
 
-	var List = function (data) {
-		this.name = ko.observable(data.name);
-		this.notes = ko.observable(data.notes);
-		this.songList = ko.observableArray(data.songList);
-	};
+	$.when(d1, d2).done(function(semesters, songs) {
+		init();
+	});
 
 
-	var ViewModel = function () {
-		var self = this;
-		this.pages = ["editing", "semester"];
-
-		// create the masterSongList
-		this.masterSongList = ko.observableArray([]);
-		
-		// create list of Lists
-		this.lOL = ko.observableArray ([]);
-
-		// for each initial song, pass it's data through Song
-		// to make it into a song, then push it to masterSongList.
-		initialSongs.forEach(function(songdata){
-			self.masterSongList().push( new Song(songdata) );
-		});
-		
-		// populate the list array
-		initialClasses.forEach(function(listdata){
-			self.lOL().push( new List(listdata) );
-		});
-
-		// set initial data
-		this.currentPage = ko.observable('semester');
-		this.currentSong = ko.observable(this.masterSongList()[0]);
-		this.currentList = ko.observable(this.lOL()[0]);
+	function init() {
+		console.log(app.model);
 
 		
-		this.setSong = function (clickedSong) {
-			self.currentSong(clickedSong);
-			self.currentPage('planning');
-		};
-		
-		this.setList = function (clickedList) {
-			self.currentList(clickedList);
+		var Song = function (data) {
+			this.songName = ko.observable(data.songName);
+			this.songUrl = ko.observable(data.songUrl);
+			this.key = ko.observable(data.key);
 		};
 
-		this.addToCurrentList = function () {
-			self.currentList().songList.push( self.currentSong() );
-			console.log(self.currentList());
+
+		var List = function (data) {
+			this.name = ko.observable(data.name);
+			this.notes = ko.observable(data.notes);
+			this.songList = ko.observableArray(data.songList);
 		};
 
-		this.togglePlanningView = function () {
-			self.planningView( !self.planningView() );
+
+		var ViewModel = function () {
+			var self = this;
+			
+
+			// UI State
+			this.pages = ["editing", "semester"];
+			this.currentPage = ko.observable('semester');
+
+
+			// Populate our song data:
+			this.masterSongList = ko.observableArray([]);
+
+			app.model.songs.forEach(function(songdata){
+				self.masterSongList().push( new Song(songdata) );
+			});
+			
+
+			// Populate our semesters data:
+			this.semesters = ko.observableArray([]);
+			ko.mapping.fromJS(app.model.semesters, {}, this.semesters);
+
+
+			app.model.semesters.forEach(function(semester){
+				semester.classes.forEach(function(listdata){
+					self.semesters().push( new List(listdata) );
+				});
+			});
+
+
+			// set current variables
+			this.currentSemester = ko.observable(this.semesters()[0]);
+			this.currentClass = ko.observable(this.currentSemester().classes()[0]);
+			this.currentSong = ko.observable(this.masterSongList()[0]);
+
+			
+			this.setSong = function (clickedSong) {
+				self.currentSong(clickedSong);
+				self.currentPage('planning');
+			};
+			
+			this.setList = function (clickedList) {
+				self.currentClass(clickedList);
+			};
+
+			this.addToCurrentClass = function () {
+				self.currentClass().songList.push( self.currentSong() );
+				console.log(self.currentClass());
+			};
+
+			this.togglePlanningView = function () {
+				self.planningView( !self.planningView() );
+			};
+
 		};
 
-	};
+		ko.applyBindings( new ViewModel() );
 
-	ko.applyBindings( new ViewModel() );
-
-})();
+	}
+// })();
