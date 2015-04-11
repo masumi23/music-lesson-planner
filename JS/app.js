03b8c0a00ce44406112472778b29b00b0140889b
Masumi Hayashi-Smith <masumi.hs@gmail.com>
2015-04-11 10:41:37 -0700

    big refactor, song instances properly inherit

--- JS/app.js
+++ JS/app.js
@@ -1,187 +1,244 @@
-// (function(){
-// connecting things to json
-	var app = {
-		model: {
-			masterSongList: ko.observableArray([])
-		}
+var app = {
+	model: {
+		masterSongList: ko. observableArray([])
+	}
+};
+
+// deferred functions in order to load json. Pushing the json into the model
+var d1 = $.Deferred();
+var d2 = $.Deferred();
+var j1 = $.getJSON("semesters.json").done(function(data){
+	app.model.semesters = data;
+	d1.resolve();
+});
+var j2 = $.getJSON("songs.json").done(function(data){
+	app.model.songs = data;
+	d2.resolve();
+});
+
+$.when(d1, d2).done(function(semesters, songs) {
+	init();
+});
+
+// rest of code
+
+function init () {
+
+	// turn things from .json data into objects that we can use.
+	// there are new Song, SongInstance, and Class functions later - 
+	// hence capitalized
+	var Song = function (data) {
+		this.songName = ko.observable(data.songName);
+		this.songUrl = ko.observable(data.songUrl);
+		this.key = ko.observable(data.key);
+		this.uid = ko.observable(data.uid);
+		this.notes = ko.observable(data.notes);
 	};
 
-
-	var d1 = $.Deferred();
-	var d2 = $.Deferred();
-	var j1 = $.getJSON("semesters.json").done(function(data){
-		app.model.semesters = data;
-		d1.resolve();
-	});
-	var j2 = $.getJSON("songs.json").done(function(data){
-		app.model.songs = data;
-		d2.resolve();
-	});
-
-	$.when(d1, d2).done(function(semesters, songs) {
-		init();
-	});
-
-//the rest of the code
-	function init() {
-		console.log("init"+app.model);
-
+	// Song instances can have their own versions of properties, but fall
+	// back onto the associated song (by uid) in the song list when a
+	// property doesn't have an explicit value set.
+	var SongInstance = function (data) {
+		var allSongs = app.model.masterSongList();
 		
-		var Song = function (data) {
-			this.songName = ko.observable(data.songName);
-			this.songUrl = ko.observable(data.songUrl);
-			this.key = ko.observable(data.key);
-			this.uid = ko.observable(data.uid);
-			this.notes = ko.observable(data.notes);
+		// Find the associated song with this song instance, searching by uid
+		for (var i=0; i < allSongs.length; i++) {
+			if (allSongs[i].uid() === data.uid) {
+				console.log("matched uid");
+				this.associatedSong = ko.observable(allSongs[i]);
+				break;
+			}
+		}
+
+		// Each song property gets its own instance observable version.
+		// When the property is read, it tries to get its own version
+		// of the property, falling back on its associated song's version
+		// of the property. This allows for individual songs to have their
+		// own custom properties, but they "inherit" from the song in the
+		// master song list.
+		// TODO: generalize this functionality so we don't have to copy
+		// and paste for any new properties we want.
+		this.instanceSongName = ko.observable(data.songName);
+		this.songName = ko.pureComputed({
+			read: function() {
+				return this.instanceSongName() || this.associatedSong().songName();
+			},
+			write: function(value) {
+				this.instanceSongName(value);
+			},
+			owner: this
+		});
+		
+		this.instanceSongUrl = ko.observable(data.songUrl);
+		this.songUrl = ko.pureComputed({
+			read: function() {
+				return this.instanceSongUrl() || this.associatedSong().songUrl();
+			},
+			write: function(value) {
+				this.instanceSongUrl(value);
+			},
+			owner: this
+		});
+		
+		this.instanceKey = ko.observable(data.key);
+		this.key = ko.pureComputed({
+			read: function() {
+				return this.instanceKey() || this.associatedSong().key();
+			},
+			write: function(value) {
+				this.instanceKey(value);
+			},
+			owner: this
+		});
+		
+		this.instanceUid = ko.observable(data.uid);
+		this.uid = ko.pureComputed({
+			read: function() {
+				return this.instanceUid() || this.associatedSong().uid();
+			},
+			write: function(value) {
+				this.instanceUid(value);
+			},
+			owner: this
+		});
+		
+		this.instanceNotes = ko.observable(data.notes);
+		this.notes = ko.pureComputed({
+			read: function() {
+				return this.instanceNotes() || this.associatedSong().notes();
+			},
+			write: function(value) {
+				this.instanceNotes(value);
+			},
+			owner: this
+		});
+	};
+
+	// 
+	var Class = function (data) {
+		var self = this;
+
+		this.date = ko.observable(data.date);
+		this.name = ko.observable(data.name);
+		this.materials = ko.observable(data.materials);
+		this.notes = ko.observable(data.notes);
+		this.songs = ko.observableArray();
+
+		// for each semesters[i].classes[i].songs, we are creating a new 
+		// "SongInstance" that contains all the original data from the
+		// "Song" plus any additional info for that instance.
+		data.songs.forEach(function(songData){
+			console.log(songData);
+			self.songs.push( new SongInstance(songData) );
+			//returns as unidentified
+			console.log(self.songs()[0].songName());
+		});
+		
+	};
+
+	// 
+	var ViewModel = function () {
+		var self = this;
+
+		// UI State
+		this.pages = ["editing", "semester", "review"];
+		this.reviewDisplays = ["song", "class"];
+		this.currentPage = ko.observable('review');
+		this.reviewDisplay = ko.observable('song');
+
+		// bring the masterSongList into the VM
+		this.masterSongList = app.model.masterSongList();
+		
+		//  create semesters array
+		this.semesters = ko.observableArray([]);
+
+		// populate masterSongList() with "Song"s converted from the model
+		app.model.songs.forEach(function(songdata){
+			app.model.masterSongList().push( new Song(songdata) );
+		});
+
+		// for each object in model.semester, create a semesterKO object
+		// which has an observable array of "Class"es.
+		app.model.semesters.forEach(function(semester){
+			var semesterClasses = ko.observableArray([]);
+		
+			// for each object in "app.model.semester.classes", push its 
+			// data (as a new "Class") into the classes property on the
+			// now observableArray for semesters ("obj")
+			semester.classes.forEach(function(classData){
+				semesterClasses.push( new Class(classData) );
+			});
+		
+			// Push a semester object into ViewModel.semesters.
+			self.semesters.push({
+				classes: semesterClasses
+			});
+		});
+
+		// keep track of the state of the application
+		this.currentSemester = ko.observable(this.semesters()[0]);
+		//If I write the following as "this.currentSemester().semesterKO.classes" 
+		//it loads as "undefined is not a function". I don't understand why I don't
+		//use "semesterKO"?
+		this.currentClass = ko.observable(this.currentSemester().classes()[0]);
+		this.currentSong = ko.observable(app.model.masterSongList()[0]);
+
+		//functions for setting different current___ offten associated
+		//with the data-bind, "click".
+		this.setPage = function (clickedPage) {
+			self.currentPage(clickedPage);
 		};
 
-		var SongInstance = function (data) {
-			this.songName = ko.observable(data.songName);
-			this.songUrl = ko.observable(data.songUrl);
-			this.key = ko.observable(data.key);
-			this.uid = ko.observable(data.uid);
-			this.notes = ko.observable(data.notes);
-			this.data = ko.computed(function() {
-				var allSongs = app.model.masterSongList();
-				// debugger;
-				for (var i=0; i < allSongs.length; i++) {
-					if (allSongs[i].uid === this.uid) {
-						console.log(allSongs[i]);
-						return allSongs[i];
-						// Class.songs.push(allSongs[i]);
-						// console.log(allSongs[i]);
-					}
-				}
-			});
-
-			var getSongData = function (uid) {
-				
-			};
+		this.setSong = function (clickedSong) {
+			self.currentSong(clickedSong);
+			self.currentPage('review');
+			self.reviewDisplay('song');
+		};
+		
+		this.setClass = function (clickedList) {
+			self.currentClass(clickedList);
 		};
 
-
-		var Class = function (data) {
-			var self = this;
-
-			this.date = ko.observable(data.date);
-			this.name = ko.observable(data.name);
-			this.materials = ko.observable(data.materials);
-			this.notes = ko.observable(data.notes);
-			this.songs = ko.observableArray();
-
-			data.songs.forEach(function(songData){
-				console.log(songData);
-				self.songs.push( new SongInstance(songData) );
-			});
-			//getting the songs by UID
-			//very confused
-			
-			// findSongs(data.songs);
-			// console.log("hi"+Class(app.model.semesters[0].classes[0]));
+		this.addToCurrentClass = function () {
+			self.currentClass().songs.push( self.currentSong() );
+			console.log(self.currentClass());
 		};
 
-
-		// var findSongs = function (dSongList) {
-		// 	var allSongs = ViewModel.masterSongList;
-		// 	for (i=0; i < dSongList; i++) {
-		// 		classSongUid = dSongList[i].uid;
-		// 		var matchSongs = function (classSongUid) {
-		// 			for (var i=0; i < allSongs; i++) {
-		// 				if (allSongs[i].uid == classSongUid) {
-		// 					Class.songs.push(allSongs[i]);
-		// 					console.log(allSongs[i]);
-		// 				}
-		// 			}
-		// 		};
-		// 	}
-		// };
-
-
-		var ViewModel = function () {
-			var self = this;
-			
-
-			// UI State
-			this.pages = ["editing", "semester", "review"];
-			this.reviewDisplays = ["song", "class"];
-			this.currentPage = ko.observable('review');
-			this.reviewDisplay = ko.observable('song');
-
-			this.masterSongList = app.model.masterSongList;
-			
-			
-			// Populate our semesters data:
-			this.semesters = ko.observableArray([]);
-
-
-			// ko.mapping.fromJS(app.model.semesters, {
-			// 	songs: {
-			// 		create: function(options) {
-			// 			if (!options.data) {
-			// 				return null;
-			// 			}
-						
-			// 			options.data.data = 'poop';
-			// 			return ko.mapping.fromJS(options.data);
-			// 		}
-			// 	}
-			// }, this.semesters);
-
-			app.model.songs.forEach(function(songdata){
-				app.model.masterSongList().push( new Song(songdata) );
-			});
-			
-
-			app.model.semesters.forEach(function(semester){
-				var semesterKO = {
-					classes: ko.observableArray([])
-				};
-
-				semester.classes.forEach(function(classData){
-					semesterKO.classes.push( new Class(classData) );
-				});
-
-				self.semesters.push(semesterKO)
-
-			});
-
-
-			// set current variables
-			this.currentSemester = ko.observable(this.semesters()[0]);
-			this.currentClass = ko.observable(this.currentSemester().classes()[0]);
-			this.currentSong = ko.observable(app.model.masterSongList()[0]);
-
-			
-			this.setPage = function (clickedPage) {
-				self.currentPage(clickedPage);
-			};
-
-			this.setSong = function (clickedSong) {
-				self.currentSong(clickedSong);
-				self.currentPage('review');
-				self.reviewDisplay('song');
-			};
-			
-			this.setClass = function (clickedList) {
-				self.currentClass(clickedList);
-			};
-
-			this.addToCurrentClass = function () {
-				self.currentClass().songs.push( self.currentSong() );
-				console.log(self.currentClass());
-			};
-
-			this.togglePlanningView = function () {
-				self.planningView( !self.planningView() );
-			};
-
-			this.setReviewDisplayClass = function () {
-				self.reviewDisplay('class');
-			};
-
+		this.togglePlanningView = function () {
+			self.planningView( !self.planningView() );
 		};
 
-		ko.applyBindings( new ViewModel() );
+		this.setReviewDisplayClass = function () {
+			self.reviewDisplay('class');
+		};
+	};
 
-	}
-// })();
+	ko.applyBindings( new ViewModel() );
+
+}
+
+/* * Glossary * *
+
+ * "masterSongList" - ko.observableArray that is composed of "Songs" from .json.
+		There are two instances: "app.model.masterSongList()" and 
+		"ViewModel.masterSongList" which just referrs to the former
+
+ * "semesters" - array that has three instances. First as a .json file, then as 
+		app.model.semesters, and finally as ViewModel.semesters()
+		(a ko.observableArray).
+
+ * "Song" - we pass songs from "songs.json" into it to transform everything into
+		ko.observables, and then we will put these into the masterSongList.
+
+ * "SongInstance" - this will be specifically for instances of songs in classes.
+		The difference is that the instances might have slightly altered
+		information that I want to track.
+
+ * "Class" - This makes a class with data from "semesters.json". We pass in
+		semesters[i].classes[i]. The array of "Class.songs" is actually
+		composed of "SongInstance"s, not "Song"s
+
+ * "classes" - ko.observableArray made up of "Class"es and housed in
+		"ViewModel.semesters
+
+ */
